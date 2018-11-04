8297460ccba444b7ebe5ca4bb4baf1897ffbd404
MiltonMatias22 <nodout.develops@gmail.com>
2018-11-04 15:21:55 -0200

    Nav-bar vue component

---
+++ resources/js/components/NavTopComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" v-bind:href="url">{{titulo}}</a>
+    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+        <ul class="navbar-nav ml-auto mr-5">
+            <slot name="menu-nav-content"></slot>
+        </ul>
+    </div>
+</nav>
+
+    
+</template>
+
+<script>
+export default {
+  props: ["titulo", "url"]
+};
+</script>
+
